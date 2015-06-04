86cc5892f694bba57407250f4ffb5e3edd014543
sentausa <erwin@ncy-iam-esentausa.local>
2015-06-04 17:27:44 +0200

    Plot3 without copying

--- plot3.R
+++ plot3.R
@@ -14,6 +14,9 @@ library(lubridate)
 #Converting Date and Time into datetime
 data$datetime <- dmy_hms(paste(data$Date, data$Time))
 
+#Opening png device
+png(file = "plot3.png", width = 480, height = 480)
+
 #Creating the plot
 with(data, plot(datetime, Sub_metering_1, type = "n", xlab = "", ylab = "Energy sub metering"))
 with(data, points(datetime, Sub_metering_1, type = "l"))
@@ -21,6 +24,5 @@ with(data, points(datetime, Sub_metering_2, type = "l", col = "red"))
 with(data, points(datetime, Sub_metering_3, type = "l", col = "blue"))
 legend("topright", lwd = 1, col = c("black", "red", "blue"), legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"))
 
-#Copying the plot to the png file
-dev.copy(png, file = "plot3.png", width = 480, height = 480)
+#Closing the device
 dev.off()
